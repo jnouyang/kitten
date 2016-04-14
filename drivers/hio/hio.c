ba7d0fe8fea339d8b1faf61843d9653e3b10c41b
Jiannan Ouyang <ouyang@cs.pitt.edu>
2016-04-13 21:56:46 -0400

    hio: kitten->linux syscall forwarding is working
    TODO: linux and kitten spinlocks are not compatible

--- drivers/hio/hio.c
+++ drivers/hio/hio.c
@@ -23,22 +23,26 @@
 #include "hio.h"
 
 // hardcoded STUB_ID currently support on process only
-#define STUB_ID 123
+#define STUB_ID 3
 
 static int forward_syscall(int syscall_nr, uint64_t arg0, uint64_t arg1, 
+        uint64_t arg2, uint64_t arg3, uint64_t arg4); 
+static int insert_syscall(int syscall_nr, uint64_t arg0, uint64_t arg1, 
         uint64_t arg2, uint64_t arg3, uint64_t arg4); 
 
 static struct hio_engine *engine = NULL;
 
-struct pending_syscalls {
+struct pending_ret {
     int stub_id;
+    spinlock_t lock;
+
     waitq_t waitq;
 
     int ret_val;
     int is_pending;
 };
 
-static struct pending_syscalls pending_syscall_array[MAX_STUBS];
+static struct pending_ret pending_ret_array[MAX_STUBS];
 
 static int
 hio_open(struct inode *inode, struct file *file) {
@@ -51,13 +55,76 @@ hio_release(struct inode * inodep, struct file  * filp) {
     return 0;
 }
 
+static void dump_page(void *buf) {
+    unsigned long long *ptr = buf;
+    int i, j;
+    int col_num = 8;
+
+    printk(KERN_INFO "magic %p\n", &engine->magic);
+    printk(KERN_INFO "prod_idx %p\n", &engine->rb_syscall_prod_idx);
+    printk(KERN_INFO "cons_idx %p\n", &engine->rb_syscall_cons_idx);
+    printk(KERN_INFO "ret prod_idx %p\n", &engine->rb_ret_prod_idx);
+    printk(KERN_INFO "ret cons_idx %p\n", &engine->rb_ret_cons_idx);
+
+    // 4KB page is 512 * 8bytes
+    for (i = 0; i < 512/col_num; i++) {
+        for (j = 0; j < col_num; j++) {
+            printk(KERN_INFO "%llx ", *ptr);
+            ptr++;
+        }
+        printk(KERN_INFO "\n");
+    }
+}
+
+
 static void engine_dispachter_loop(void) {
-    printk("Engine dispather loop...\n");
-    return;
+    printk(KERN_INFO "Enter HIO engine dispather loop...\n");
+
+    if (engine == NULL) {
+        printk(KERN_ERR "engine is NULL\n");
+    }
+
+    dump_page((void *)engine);
+    printk(KERN_INFO "Test fording getpid syscall\n");
+    insert_syscall(39, 0, 0, 0, 0, 0);
+
     do {
+        while (engine->rb_ret_prod_idx != engine->rb_ret_cons_idx) {
+            spin_lock(&engine->lock);
+
+            if (engine->rb_ret_prod_idx == engine->rb_ret_cons_idx)
+                break;
+
+            struct hio_cmd_t *cmd = &(engine->rb[engine->rb_ret_cons_idx]);
+
+            printk(KERN_INFO "ENGINE dispatcher: consume ret index %d (prod index %d)\n", 
+                    engine->rb_ret_cons_idx,
+                    engine->rb_ret_prod_idx);
+
+            struct pending_ret *ret = &pending_ret_array[cmd->stub_id];
+
+            if (ret->is_pending) {
+                printk(KERN_ERR "Another ret is pending for stub_id %d???\n", cmd->stub_id);
+                goto out;
+            } else {
+                spin_lock(&ret->lock);
+                ret->ret_val = cmd->ret_val;
+                printk(KERN_INFO "Return value is %d\n", ret->ret_val);
+                ret->is_pending = true;
+                spin_unlock(&ret->lock);
+            }
+
+out:
+            engine->rb_ret_cons_idx = (engine->rb_ret_cons_idx + 1) % HIO_RB_SIZE;
+            spin_unlock(&engine->lock);
+
+            wake_up_interruptible(&ret->waitq);
+        } 
+
         schedule();
     } while(1);
 }
+
 /*
  * User ioctl to the HIO driver. 
  */
@@ -165,7 +232,7 @@ hio_getsockopt(uint64_t arg0, uint64_t arg1,
 }
 
 static int
-forward_syscall(
+insert_syscall(
 	int    syscall_nr,
 	uint64_t    arg0,
 	uint64_t    arg1,
@@ -173,10 +240,8 @@ forward_syscall(
 	uint64_t    arg3,
 	uint64_t    arg4
 ) {
-    printk(KERN_INFO "Forward syscall\n");
-
     /* Assume Linux and Kitten has compatible spinlocks*/
-    spin_lock(&engine->lock);
+    //spin_lock(&engine->lock);
     if ((engine->rb_syscall_prod_idx + 1) % HIO_RB_SIZE == engine->rb_ret_cons_idx) {
         spin_unlock(&engine->lock);
         printk(KERN_ERR "Error: engine ringbuffer is full\n");
@@ -193,12 +258,28 @@ forward_syscall(
     cmd->arg4 = arg4;
     engine->rb_syscall_prod_idx = (engine->rb_syscall_prod_idx + 1) % HIO_RB_SIZE;
 
-    spin_unlock(&engine->lock);
+    //spin_unlock(&engine->lock);
+    return 0;
+}
 
-    struct pending_syscalls *pending = &pending_syscall_array[cmd->stub_id];
-    wait_event_interruptible(pending->waitq, pending->is_pending);
+static int
+forward_syscall(
+	int    syscall_nr,
+	uint64_t    arg0,
+	uint64_t    arg1,
+	uint64_t    arg2,
+	uint64_t    arg3,
+	uint64_t    arg4
+) {
+    printk(KERN_INFO "Forward syscall %d\n", syscall_nr);
 
-    return pending->ret_val;
+    if (insert_syscall(syscall_nr, arg0, arg1, arg2, arg3, arg4) < 0)
+        return -1;
+
+    struct pending_ret *ret = &(pending_ret_array[STUB_ID]);
+    wait_event_interruptible(ret->waitq, ret->is_pending);
+
+    return ret->ret_val;
 }
 
 /*
@@ -223,7 +304,9 @@ hio_module_init(void) {
     */
 
     for (i = 0; i < MAX_STUBS; i++) {
-        waitq_init(&pending_syscall_array[i].waitq);
+        pending_ret_array[i].is_pending = false;
+        waitq_init(&pending_ret_array[i].waitq);
+        spin_lock_init(&pending_ret_array[i].lock);
     }
 
     kfs_create("/dev/hio",
